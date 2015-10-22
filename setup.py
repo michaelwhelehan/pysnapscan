81717b4d0ec5848eab73ba374bdb9832a613e957
Michael Whelehan <michael.marula@gmail.com>
2015-10-22 23:03:00 +0200

    changed license to beerware in setup

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='pysnapscan',
     version='0.0.1',
     description="Python bindings for SnapScan's REST API",
-    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
+    long_description = open('README.md', 'r').read() + open('AUTHORS.rst', 'r').read(),
     author='Michael Whelehan',
     author_email='dev@unomena.com',
-    license='BSD',
+    license='Beerware',
     url='https://github.com/michaelwhelehan/pysnapscan.git',
     packages = find_packages(),
     dependency_links = [],
@@ -16,17 +16,15 @@ setup(
     ],
     tests_require=[
         'django-setuptest>=0.1.2',
-        'pysqlite>=2.5',
-	    'pycurl'
+	    'requests'
     ],
     test_suite="setuptest.setuptest.SetupTestSuite",
     include_package_data=True,
     classifiers = [
         "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta",
+        "License :: Beerware",
+        "Development Status :: 0 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Django",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
